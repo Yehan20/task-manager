aa154d25e319887d0e1b976ea303114a296bca87
Yehan20 <yehansk15@gmail.com>
2025-07-03 11:10:28 +0530

    feat: add authentication methods (registar,login,logout,refresh token, get user)

---
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+
+    /**
+     * Get a JWT and the Logged user via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(LoginRequest $request)
+    {
+
+        $credentials = $request->validated();
+
+        $token = Auth::attempt($credentials);
+
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'data' => Auth::user(),
+            'authorization' => [
+                'token' => $token,
+                'type' => 'bearer'
+            ]
+        ], 200);
+    }
+
+
+    /**
+     * Get a JWT and the Registered via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(RegisterRequest $request)
+    {
+
+        $validated = $request->validated();
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password'])
+        ]);
+
+        $token = Auth::login($user);
+
+
+        return response()->json([
+            'status' => 'success',
+            'data' => Auth::user(),
+            'authorization' => [
+                'access_token' => $token,
+                'type' => 'bearer'
+            ]
+        ], 201);
+    }
+
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json([
+            'data' => Auth::user()
+        ]);
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout(true); // Allow token to be blacklisted
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->setTTL(120)->refresh(true, true));
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'authorization' => [
+                'access_token' => $token,
+                'type' => 'bearer'
+            ]
+        ]);
+    }
+}
