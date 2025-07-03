6751740b18fc984ef29253d970e5524fed2035c4
Yehan20 <yehansk15@gmail.com>
2025-07-03 23:45:20 +0530

    feat: add custom exception thrown when marking task which is already completed as complete

---
+++ backend/app/Exceptions/TaskAlreadyCompletedExpection.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+
+class TaskAlreadyCompletedExpection extends Exception
+{
+    
+    /**
+     * Report or log an exception.
+     *
+     * @return void
+     */
+    public function report()
+    {
+        Log::debug('Task is already completed');
+    }
+
+    /**
+
+     * Render the exception as an HTTP response.
+
+     */
+
+    public function render(Request $request): Response
+
+    {
+        return response(['message'=>'task is already completed','status'=>'error'],409);
+    }
+}
