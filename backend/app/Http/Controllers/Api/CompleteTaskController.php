e83086e08715bf6cb795b4bdd95519bfbf635dda
Yehan20 <yehansk15@gmail.com>
2025-07-03 23:20:48 +0530

    feat: create complete task invokable controller containg complete task method

---
+++ backend/app/Http/Controllers/Api/CompleteTaskController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Exceptions\TaskAlreadyCompletedExpection;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use App\Services\TaskService;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
+class CompleteTaskController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Task $task, TaskService $taskService)
+    {
+        Gate::authorize('update', $task);
+
+
+
+        $task = $taskService->complete($task->id);
+
+        return new TaskResource($task);
+    }
+}
