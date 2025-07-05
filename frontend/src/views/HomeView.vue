dd78ccd945767af2abe4bd670e90255d27d28e5f
Yehan20 <yehansk15@gmail.com>
2025-07-05 21:53:37 +0530

    feat: update HomeView page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
+import AppLayout from '@/layouts/AppLayout.vue';
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <AppLayout>
+    <v-container>
+      <h2>User logged {{authStore?.currentUser?.name}}</h2>
+      
+      <v-sheet color="surface-light" height="200" rounded="lg" width="100%">
+        
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam vero odio ab assumenda incidunt totam eveniet
+        laudantium ipsam blanditiis iure delectus, quasi, modi natus molestias nam praesentium sunt animi. Porro?
+      </v-sheet>
+    </v-container>
+  </AppLayout>
+
 </template>
