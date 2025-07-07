e08d98b4639e4247d61d6515cda50e95f0281a2f
Yehan20 <yehansk15@gmail.com>
2025-07-08 03:22:26 +0530

    chore:update add esbuild rule

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  esbuild:{
+   drop:['console','debugger']
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
