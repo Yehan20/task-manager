093a126b30ce978ef1b14e4368f9e42ecd9b99cd
Yehan20 <yehansk15@gmail.com>
2025-07-07 19:23:58 +0530

    feat:create new views

---
+++ frontend/src/views/NotFoundView.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import AppLayout from '@/layouts/AppLayout.vue';
+import { defineComponent } from 'vue';
+</script>
+
+<template>
+  <h1>404 Page Not found</h1>
+</template>
+
+<style scoped></style>
